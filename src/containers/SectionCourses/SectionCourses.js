93fd197749188473de1910df7651d41fef18b3b8
noor <noortshepo@gmail.com>
2021-02-25 16:31:09 +0200

    added styles file to section-courses

--- src/containers/SectionCourses/SectionCourses.js
+++ src/containers/SectionCourses/SectionCourses.js
@@ -1,9 +1,10 @@
 import React from "react";
+import * as Styles from "./SectionCourses.module.css";
 
 const SectionCourses = props => {
     console.log(props)
     return (
-        <div>
+        <div className={Styles.SectionCourses}>
             Section Courses
             <hr />
             Parameter ID - {props.match.params.id}
